c48cc744cb60401240a1002a3c7c79382b9b0af1
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-07-12 18:45:18 +0200

    Add implementation for day-07 with benchmarks and processing logic

---
+++ 2024/rust/day-07/src/part1.rs
@@ -0,0 +1,68 @@
+pub fn can_reach_target(numbers: &[usize], target: usize) -> bool {
+    fn backtrack(nums: &[usize], index: usize, current_value: usize, target: usize) -> bool {
+        if index == nums.len() {
+            return current_value == target;
+        }
+        if backtrack(nums, index +1 , current_value + nums[index], target) {
+            return true;
+        }
+        if let Some(val) = current_value.checked_mul(nums[index]) {
+            if backtrack(nums, index + 1, val, target) {
+                return true;
+            }
+        }
+        false
+    }
+    if numbers.is_empty() {
+        return false;
+    }
+    backtrack(numbers, 1, numbers[0], target)
+}
+
+pub fn process(input: &str) -> miette::Result<String> {
+
+    let results: Vec<usize> = input.lines().filter_map(|num| num.split(":").next()?.trim().parse::<usize>().ok()).collect();
+    let numbers: Vec<Vec<usize>> = input
+            .lines()
+            .filter_map(|line| {
+                line.split(":")
+                .nth(1)
+                .map(|number| {
+                    number.split_whitespace()
+                    .filter_map(|num| num.parse::<usize>().ok())
+                    .collect::<Vec<usize>>()
+                })
+            })
+            .collect();
+    let mut total: usize = 0;
+
+    for (results, numbers) in results.iter().zip(numbers.iter()) {
+        if can_reach_target(numbers, *results) {
+            total += *results;
+        }
+    }
+    
+
+
+    Ok(total.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+        assert_eq!("3749", process(input)?);
+        Ok(())
+    }
+}
